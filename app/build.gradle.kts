61fa0a9dca6a2ee9b225099237a497b33af2a1dd
Monty <allccavalenzuela@gmail.com>
2025-09-05 00:56:43 -0500

    Dependencias Retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,10 @@ dependencies {
 
     implementation("androidx.navigation:navigation-compose:2.7.5")
     implementation("io.coil-kt:coil-compose:2.5.0")
+
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.0")
 }
