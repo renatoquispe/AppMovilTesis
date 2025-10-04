1f9e3a7c437797f56c7166d0f4cfaac73ab94807
renatoquispe <quisperenato2204@gmail.com>
2025-10-03 21:17:46 -0500

    Cambio de logo y nombre del app

--- app/src/main/java/com/tesis/appmovil/data/remote/RetrofitClient.kt
+++ app/src/main/java/com/tesis/appmovil/data/remote/RetrofitClient.kt
@@ -11,7 +11,7 @@ import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
 
-    private const val BASE_URL = "https://calm-views-marry.loca.lt/api/"
+    private const val BASE_URL = "https://bitter-lights-wash.loca.lt/api/"
 
 
     private var tokenProvider: (() -> String?)? = null
