d0f2fe9bb05c9a768b6b2d89bc6e92be29c58812
Monty <allccavalenzuela@gmail.com>
2025-11-01 19:43:02 -0500

    changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     id("com.android.application") version "8.10.0" apply false
     id("com.android.library")     version "8.10.0" apply false
-
+    id("com.google.gms.google-services") version "4.4.4" apply false
     // Si usas el Kotlin Gradle Plugin en raíz, déjalo también o marca apply false:
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     // si también declaras el Kotlin JVM plugin:
